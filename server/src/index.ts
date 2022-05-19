b6e946391a3e48d625c9696229b7b304d6e1d839
aftero69 <learnsyed9@gmail.com>
2022-05-19 23:39:32 +0530

    feat(api): add rest api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,41 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  "feat(server): add node.js and typeScript backend to the application. ";
-  "BREAKING CHANGE: file structure has changed";
+const prisma = new PrismaClient();
+
+app.get("/", async (req, res) => {
+  const windows = await prisma.windows.findMany({ include: { console: true } });
+  res.json(windows);
+});
+
+app.post("/windows", async (req, res) => {
+  const { windowName } = req.body;
+  const window = await prisma.windows.create({
+    data: {
+      name: windowName,
+      console: { create: { gpio: 1, name: "Console Number 1" } },
+    },
+  });
+  res.json(window);
+});
+
+app.post("/console", async (req, res) => {
+  const { name, gpio } = req.body;
+  try {
+    const console = await prisma.console.create({
+      data: { gpio, name, windowsId: 1 },
+    });
+    res.json(console);
+  } catch (error) {
+    res.json(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 app.listen(PORT, () => {
