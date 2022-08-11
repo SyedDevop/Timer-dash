b6859774b6c8073739ffdbfbc05a4485dc5fc634
syed Devop <syeds.devops007@gmail.com>
2022-08-11 19:49:56 +0530

    feat:add pino logger

---
+++ server/src/logger/index.ts
@@ -0,0 +1,15 @@
+import Logger from "pino";
+import dayjs from "dayjs";
+
+export const log = Logger({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+  base: {
+    pid: false,
+  },
+  timestamp: () => `,"time":"${dayjs().format()}"`,
+});
