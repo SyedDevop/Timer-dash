f2b52dbbcf3f1410c8300376953dbac861a3ed49
aftero69 <learnsyed9@gmail.com>
2022-08-14 10:41:35 +0530

    feat(error):add error handling

---
+++ server/src/Error/base.error.ts
@@ -0,0 +1,41 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { Response } from "express";
+import { ValidationError } from "yup";
+import { log } from "../logger";
+
+export const onErrors = async (err: any, res: Response) => {
+  if (err instanceof PrismaClientKnownRequestError) {
+    return handelPrismaError(err, res);
+  }
+  if (err instanceof ValidationError) {
+    return handelYupError(err, res);
+  }
+  if (err instanceof Error) {
+    return handelError(err, res);
+  }
+};
+
+export const handelPrismaError = (
+  err: PrismaClientKnownRequestError,
+  res: Response
+) => {
+  const { code, name, meta } = err;
+  log.error(err);
+  return res
+    .status(400)
+    .json({ status: 400, message: [meta, { code }, { name }] });
+};
+
+export const handelYupError = (err: ValidationError, res: Response) => {
+  log.error(err.errors);
+  return res
+    .status(400)
+    .json({ error: { status: "failed", message: err.errors } });
+};
+
+export const handelError = (err: Error, res: Response) => {
+  log.error(err);
+  return res
+    .status(400)
+    .json({ error: { status: "failed", message: err.message } });
+};
