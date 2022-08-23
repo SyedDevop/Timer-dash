73b4be2a2a6aba0bb88857f695e4950cf2309c0f
aftero69 <learnsyed9@gmail.com>
2022-08-24 00:14:01 +0530

    *individual containers

--- client/src/Hooks/useSocketContext.tsx
+++ client/src/Hooks/useSocketContext.tsx
@@ -1,7 +1,7 @@
 import { useContext, createContext } from "react";
 import io, { Socket } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("http://localhost:2050");
 interface SocketContextValue {
   socket: Socket;
   startingData?: any;
